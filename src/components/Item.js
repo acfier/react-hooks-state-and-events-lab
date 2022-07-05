5ac7b1c6cc4e2e7944920881245d1206a56bc50a
acfier <acfier@gmail.com>
2022-07-05 18:28:34 -0400

    commit tree 1f5d6b27e3edd2d827fe9bd66b8d47878495a6f5

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+
+  const [inCart, setInCart] = useState(false)
+
+  const cartCss = inCart ? "in-cart" : ""
+  const buttonText = inCart ? "Remove from Cart" : "Add to Cart"
+
   return (
-    <li className="">
+    <li className={cartCss}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={inCart ? "remove" : "add"} onClick={() => setInCart(!inCart)}>{buttonText}</button>
     </li>
   );
 }
